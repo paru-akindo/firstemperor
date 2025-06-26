67dfe6934f3215d5e4c457bd20c816f0314700e0
paru-akindo <akindo125paru@gmail.com>
2025-06-26 16:40:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 # 公開シートの CSV エクスポートURL (gidは対象シートのID)
 sheet_id = "1px1zotoJ5BvWFOfyJetF4YE52hFb99QJP92CjS8OOG4"
 gid      = "0"
-csv_url  = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+csv_url  = f"https://docs.google.com/spreadsheets/d/e/2PACX-1vTFOIQtAvilcjBplaklTqvDLg5KAYA8jkU9wL8pBEnQurFp9m62Z4rqbTGWPar0Y4keCqYSjm5qTM2t/pub?gid=236779120&single=true&output=csv"
 
 # Pandas で直接読み込む
 df = pd.read_csv(csv_url)
