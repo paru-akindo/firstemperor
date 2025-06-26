c4048f931db3cbf8d89d2cc9985263d960bdf14c
paru-akindo <akindo125paru@gmail.com>
2025-06-26 16:36:13 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# 公開シートの CSV エクスポートURL (gidは対象シートのID)
+sheet_id = "1px1zotoJ5BvWFOfyJetF4YE52hFb99QJP92CjS8OOG4"
+gid      = "0"
+csv_url  = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+
+# Pandas で直接読み込む
+df = pd.read_csv(csv_url)
+
+st.title("公開スプレッドシートを無料で表示")
+st.dataframe(df)
